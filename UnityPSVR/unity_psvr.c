1e301c16d8db6a68e3c40c108dfef198898b20e4
hrl7 <hrlclb@gmail.com>
2016-10-18 21:38:02 +0900

    Initial Commit

---
+++ UnityPSVR/unity_psvr.c
@@ -0,0 +1,9 @@
+//
+//  unity_psvr.c
+//  UnityPSVR
+//
+//  Created by 寺元健太郎 on 2016/10/18.
+//  Copyright © 2016年 寺元健太郎. All rights reserved.
+//
+
+#include "unity_psvr.h"
---
+++ UnityPSVR/unity_psvr.h
@@ -0,0 +1,14 @@
+//
+//  unity_psvr.h
+//  UnityPSVR
+//
+//  Created by 寺元健太郎 on 2016/10/18.
+//  Copyright © 2016年 寺元健太郎. All rights reserved.
+//
+
+#ifndef unity_psvr_h
+#define unity_psvr_h
+
+#include <stdio.h>
+
+#endif /* unity_psvr_h */
